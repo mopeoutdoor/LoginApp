dac8a32218d36ea4d7456cd36e59d3290c4645f6
Mikhail Scherbina <irinakop@air-irina.lan>
2020-02-07 09:47:57 +0300

    Initial Commit

---
+++ LoginApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LoginApp
+//
+//  Created by Irina Kopchenova on 07.02.2020.
+//  Copyright © 2020 Mikhail Scherbina. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
